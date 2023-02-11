055733d73d1ba4c39b06d55bc2396d788ca132cf
blackd44 <irabd44@gmail.com>
2023-02-11 11:05:28 +0200

    adding router dom in project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/home.js';
+import Layout from './components/layout';
+import About from './pages/about';
 
 function App() {
   return (
     <>
-      hello
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />}></Route>
+          <Route path='/about' element={<About />}></Route>
+        </Route>
+      </Routes>
     </>
   );
 }
