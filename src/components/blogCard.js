cfe0f7ea0fe0337308b8e599afab6a6f9148399d
blackd44 <irabd44@gmail.com>
2023-02-12 13:26:53 +0200

    adding environoment valiables in the project

--- src/components/blogCard.js
+++ src/components/blogCard.js
@@ -1,9 +1,12 @@
 import { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 
+const default_image = process.env.REACT_APP_DEFAULT_IMAGE
+
 const BlogCard = ({ blog, ...props }) => {
 
     const item = useRef()
+    const image = useRef()
 
     useEffect(() => {
         if (item && item.current) {
@@ -12,12 +15,18 @@ const BlogCard = ({ blog, ...props }) => {
                 window.location.assign(`/blogs/${blog._id}`)
             })
         }
+
+        if (image.current && image.current !== null) {
+            image.current.onerror = () => {
+                image.current.src = default_image
+            }
+        }
     }, [blog._id])
 
     return (
         <>
             <li ref={item}>
-                <img alt="title" src={blog.image} />
+                <img alt="title" src={blog.image} ref={image} />
                 <div className="body">
                     <Link to={'/blogs/' + blog._id}>
                         <h2>{blog.title}</h2>
