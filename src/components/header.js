fae61b49601ef98a8ee4bb37ddaa7dc4bcb05aac
blackd44 <irabd44@gmail.com>
2023-02-12 14:45:19 +0200

    adding logout in the project

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,61 @@
-import { NavLink, useLocation } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
+import Cookie from "js-cookie";
+import { useCallback, useEffect, useRef, useState } from "react";
+import axios from "axios";
+
+const server = process.env.REACT_APP_SERVER_URL
 
 const Header = () => {
     const location = useLocation()
 
+    const [user, setUser] = useState(null)
+    const [token, setToken] = useState(Cookie.get('token'))
+
+    let signout = useRef()
+
+    const logout = useCallback(function (e) {
+        Cookie.set('token', undefined, { expires: -1 })
+        setUser(null)
+        setToken(undefined)
+        console.log(e)
+    }, [])
+
+    useEffect(() => {
+        let sign = signout.current
+        if (sign && sign !== null) {
+            sign.addEventListener('click', logout)
+        }
+
+        return () => {
+            if (sign && sign !== null) {
+                sign.removeEventListener('click', logout)
+            }
+        }
+    }, [logout, user])
+
+    useEffect(() => {
+        if (!token || token === null)
+            return
+        axios
+            .get(server + '/api/users/user', {
+                headers: {
+                    Authorization: 'Bearer ' + token
+                }
+            })
+            .then(res => {
+                if (res.status !== 204) {
+                    if (res.status === 200) {
+                        setUser(prev => res.data)
+                    }
+                    else
+                        console.log(res.data)
+                }
+            }).catch(e => console.log(e))
+    }, [token])
+
+    // useEffect(() => {
+    //     console.log(user)
+    // }, [user])
     return (
         <>
             <header>
@@ -27,17 +80,37 @@ const Header = () => {
                         <li>Contact</li>
                     </NavLink>
                     {
-                        location.pathname !== '/login'
-                            ? (
-                                <NavLink to="/login">
-                                    <li>login</li>
-                                </NavLink>
-                            )
-                            : (
-                                <NavLink to="/signup">
-                                    <li>signup</li>
-                                </NavLink>
-                            )
+                        user !== null ? (
+                            <div>
+                                <li className="profile">
+                                    <div className="profile">
+                                        <img alt={user.username} src={user.profile} />
+                                    </div>
+                                </li>
+                                <div className="submenu">
+                                    <article>
+                                        <Link to="/dashboard/profile">profile</Link>
+                                        <Link to="/dashboard/">dashboard</Link>
+                                        <Link to="/blogs/add">add blog</Link>
+                                    </article>
+                                    <article>
+                                        <Link ref={signout} to='/' style={{ color: 'rgb(255, 0, 0)' }}>signout</Link>
+                                    </article>
+                                </div>
+                            </div>
+                        ) : (
+                            location.pathname !== '/login'
+                                ? (
+                                    <NavLink to="/login">
+                                        <li>login</li>
+                                    </NavLink>
+                                )
+                                : (
+                                    <NavLink to="/signup">
+                                        <li>signup</li>
+                                    </NavLink>
+                                )
+                        )
                     }
                 </ul>
                 <script type="module" defer src="/js/munubar.js"></script>
