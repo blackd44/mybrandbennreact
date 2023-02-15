6e1dbe2b0e5b8eee440f2b3a591d7e4470ab3873
blackd44 <irabd44@gmail.com>
2023-02-15 14:06:42 +0200

    adding the loaders

---
+++ src/components/loaders/inlinedots/inlinedots.js
@@ -0,0 +1,16 @@
+import style from './style.module.css'
+
+const InlineDots = () => {
+    return (
+        <>
+            <div class={style.ldsEllipsis}>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </>
+    );
+}
+
+export default InlineDots;
