41d41f54b760c98469a117d2f4220500cfb71ff0
blackd44 <irabd44@gmail.com>
2023-02-16 21:51:09 +0200

    adding some blog style

--- src/pages/dashboard/blogs.js
+++ src/pages/dashboard/blogs.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import InlineDots from "../../components/loaders/inlinedots/inlinedots";
 
 
 let server = process.env.REACT_APP_SERVER_URL
@@ -41,30 +42,43 @@ const DashBlogs = () => {
             <div className="list blog">
                 <hr />
                 <ul>{
-                    blogs.map(blog => (
-                        <li key={blog._id}>
-                            <Link to={"/blogs/" + blog._id}>
-                                <h2>{blog?.title || 'unknown'}</h2>
-                            </Link>
-                            <div className="flex-between">
-                                <span className="grey-color">{blog?.owner?.username || 'unknown'}</span>
-                                <span>
-                                    <Link to={"/dashboard/blogs/edit/" + blog._id}>
-                                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M23.167 5.8002C23.6598 5.3127 23.6598 4.5002 23.167 4.0377L20.2104 1.1127C19.7429 0.625195 18.9217 0.625195 18.4289 1.1127L16.1041 3.4002L20.8422 8.0877M0.790527 18.5627V23.2502H5.52863L19.5029 9.4127L14.7648 4.7252L0.790527 18.5627Z" fill="#1D1D20">
-                                            </path>
-                                        </svg>
+                    blogs.length === 0 ? (
+                        <>
+                            <InlineDots />
+                        </>
+                    ) : (
+                        blogs.map(blog => (
+                            <li key={blog._id}>
+                                <img src={blog?.image} alt={blog?.title} />
+                                <aside>
+                                    <Link to={"/blogs/" + blog._id}>
+                                        <h2>{blog?.title || 'unknown'}</h2>
                                     </Link>
-                                    <Link>
-                                        <svg width="19" height="24" viewBox="0 0 19 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M18.6217 2H14.1994L12.9359 0.75H6.61846L5.35497 2H0.932739V4.5H18.6217M2.19623 20.75C2.19623 21.413 2.46247 22.0489 2.93637 22.5178C3.41027 22.9866 4.05302 23.25 4.72322 23.25H14.8312C15.5014 23.25 16.1441 22.9866 16.618 22.5178C17.0919 22.0489 17.3582 21.413 17.3582 20.75V5.75H2.19623V20.75Z" fill="#1D1D20">
-                                            </path>
-                                        </svg>
-                                    </Link>
-                                </span>
-                            </div>
-                        </li>
-                    ))
+                                    <div className="flex-between">
+                                        <div>
+                                            <span className="grey-color">{blog?.owner?.username || 'unknown'}</span>
+                                            <p className="body" dangerouslySetInnerHTML={{ __html: blog?.content }}>
+                                            </p>
+                                        </div>
+                                        <span>
+                                            <Link to={"/dashboard/blogs/edit/" + blog._id}>
+                                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M23.167 5.8002C23.6598 5.3127 23.6598 4.5002 23.167 4.0377L20.2104 1.1127C19.7429 0.625195 18.9217 0.625195 18.4289 1.1127L16.1041 3.4002L20.8422 8.0877M0.790527 18.5627V23.2502H5.52863L19.5029 9.4127L14.7648 4.7252L0.790527 18.5627Z" fill="#1D1D20">
+                                                    </path>
+                                                </svg>
+                                            </Link>
+                                            <Link>
+                                                <svg width="19" height="24" viewBox="0 0 19 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M18.6217 2H14.1994L12.9359 0.75H6.61846L5.35497 2H0.932739V4.5H18.6217M2.19623 20.75C2.19623 21.413 2.46247 22.0489 2.93637 22.5178C3.41027 22.9866 4.05302 23.25 4.72322 23.25H14.8312C15.5014 23.25 16.1441 22.9866 16.618 22.5178C17.0919 22.0489 17.3582 21.413 17.3582 20.75V5.75H2.19623V20.75Z" fill="#1D1D20">
+                                                    </path>
+                                                </svg>
+                                            </Link>
+                                        </span>
+                                    </div>
+                                </aside>
+                            </li>
+                        ))
+                    )
                 }</ul>
             </div>
         </>
